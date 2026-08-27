dc9d2c7d81f521e510c2667e5c999702fa38ac60
“William” <williamg28@gmail.com>
2026-08-27 18:41:51 -0500

    Sign in updates

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,15 +1,39 @@
-import { Form } from "react-router";
+import { Form, useFetcher, useNavigate, useNavigation, type ActionFunctionArgs } from "react-router";
 
+export async function loginAction({ request }: ActionFunctionArgs) {
+    const formData = await request.formData();
+    const method = request.method.toUpperCase();
+
+    console.log(formData.get('firstName'))
+
+
+
+}
 
 export function loginLoader() {
-    
+
 }
 
 export default function Login() {
+    const fetcher = useFetcher<typeof loginAction>();
+    const disabled = false;
+    // const disabled = fetcher.state === 'submitting' || fetcher.state === 'loading';
+    
 
     return (
-        <Form>
-            
-        </Form>
+        <div className="login-form-container">
+            <h1 className="form-heading">Welcome back!</h1>
+            <fetcher.Form className="login-form" method="post">
+                <div>
+                    <input type="email" id="email" name="email" placeholder="email"></input>
+                </div>
+                <div>
+                    <input type="password" id="password" name="password" placeholder="password"></input>
+                </div>
+                <button type="submit" disabled={disabled}>Login</button>
+            </fetcher.Form>
+
+        </div>
     )
 }
+
